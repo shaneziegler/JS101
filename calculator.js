5205c57385d6b5bab0a6b3ec86d836cc4c9c68e0
Shane-Ziegler <shanedb@gmail.com>
2021-06-11 08:23:26 -0500

    done for today

---
+++ calculator.js
@@ -0,0 +1,14 @@
+// Ask the user for the first number
+// Ask the user for the second number
+// Get the operation to perform
+// Perform the calculation 
+// Output code to the console
+
+const readline = require('readline-sync');
+
+console.log("Welcome to the calculator!");
+
+// console.log("Whats the first number?");
+
+let number1 = readline.question("Whats the first number?");
+console.log(number1);
