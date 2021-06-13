5966d34a755e445b182f6ef010c1702f0a9d58e6
Shane-Ziegler <shanedb@gmail.com>
2021-06-13 14:01:11 -0500

    done

--- calculator.js
+++ calculator.js
@@ -12,3 +12,22 @@ console.log("Welcome to the calculator!");
 
 let number1 = readline.question("Whats the first number?");
 console.log(number1);
+
+let number2 = readline.question("Whats the second number?");
+console.log(number2);
+
+console.log('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide');
+let operation = readline.question();
+
+let output;
+if (operation === '1') {
+    output = Number(number1) + Number(number2);
+} else if (operation === '2') {
+    output = Number(number1) - Number(number2);
+} else if (operation === '3') {
+    output = Number(number1) * Number(number2);
+} else if (operation === '4') {
+    output = Number(number1) / Number(number2);
+}
+
+console.log(`The result is: ${output}`);
