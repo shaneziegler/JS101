3572e8a6804ad3a122a2871807bf55f10d46e954
Shane-Ziegler <shanedb@gmail.com>
2021-06-13 14:24:02 -0500

    init

---
+++ pseudocode.js
@@ -0,0 +1,3 @@
+// a function that returns the sum of two numbers
+// a function that takes an array of strings, and returns a string that is all those strings concatenated together
+// a function that takes an array of integers, and returns a new array with every other element
