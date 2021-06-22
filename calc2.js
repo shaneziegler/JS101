adc3a1dd6d16a5421e48d44d9751fe5413e857ab
Shane-Ziegler <shanedb@gmail.com>
2021-06-21 22:24:38 -0500

    Init

---
+++ calc2.js
@@ -0,0 +1,35 @@
+// Refactor old calc program
+
+// Ask the user for the first number
+// Ask the user for the second number
+// Get the operation to perform
+// Perform the calculation
+// Output code to the console
+
+const readline = require('readline-sync');
+
+console.log("Welcome to the calculator!");
+
+// console.log("Whats the first number?");
+
+let number1 = readline.question("Whats the first number?");
+console.log(number1);
+
+let number2 = readline.question("Whats the second number?");
+console.log(number2);
+
+console.log('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide');
+let operation = readline.question();
+
+let output;
+if (operation === '1') {
+  output = Number(number1) + Number(number2);
+} else if (operation === '2') {
+  output = Number(number1) - Number(number2);
+} else if (operation === '3') {
+  output = Number(number1) * Number(number2);
+} else if (operation === '4') {
+  output = Number(number1) / Number(number2);
+}
+
+console.log(`The result is: ${output}`);
