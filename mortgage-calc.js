0bf5bb9f29cc537815aac8d76f9bc8509efcdceb
Shane-Ziegler <shanedb@gmail.com>
2021-06-29 06:52:37 -0500

    Init

---
+++ mortgage-calc.js
@@ -0,0 +1,35 @@
+// JS101 - Assigment
+// Mortgage Calculator
+
+// PEDAC
+// PEDAC stands for “[Understand the] Problem, Examples / Test Cases, Data Structure, Algorithm, and Code.” 
+// PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
+
+// Process the Problem
+// - Understand the Problem
+// - Identify expected input and output
+// - Make the requirements explicit
+// - Identify rules
+// - Mental model of the problem (optional)
+
+// Examples/Test Case
+// - Validate understanding of the problem
+
+// Data Structure
+// - How we represent data that we will work with when converting the input to output.
+
+// Algorithm
+// - Steps for converting input to output
+
+// Code with Intent
+// - Code
+// - Implementation of Algorithm
+
+
+
+// Inputs:
+//
+
+// Outputs:
+//
+
