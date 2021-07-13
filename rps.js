68541075fd531f14a6ea5533306d7fd09fef50f4
Shane-Ziegler <shanedb@gmail.com>
2021-07-12 22:46:43 -0500

    Monday night

--- rps.js
+++ rps.js
@@ -1,14 +1,19 @@
 const readline = require('readline-sync');
 const VALID_CHOICES = ['rock', 'paper', 'scissors'];
 
-function prompt(msg) {
-  console.log(`=> ${msg}`);
+function prompt(message) {
+  console.log(`=> ${message}`);
 }
 
 prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
 let choice = readline.question();
 
 while (!VALID_CHOICES.includes(choice)) {
-  prompt("That's not a valid choice.");
+  prompt("That's not a valid choice");
   choice = readline.question();
 }
+
+let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
+let computerChoice = VALID_CHOICES[randomIndex];
+
+prompt(`You chose ${choice}, computer chose ${computerChoice}`);
