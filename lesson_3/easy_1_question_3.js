11c0f7ac0a247700a87090c0f42f22bafa6ff11f
Shane-Ziegler <shanedb@gmail.com>
2021-07-15 08:30:16 -0500

    Done

---
+++ lesson_3/easy_1_question_3.js
@@ -0,0 +1,10 @@
+let munstersDescription = "the Munsters are CREEPY and Spooky.";
+// => The munsters are creepy and spooky.
+
+let newMun = munstersDescription[0].toUpperCase() + munstersDescription.toLowerCase().slice(1);
+
+console.log(newMun);
+
+
+//munstersDescription.charAt(0).toUpperCase() +
+//  munstersDescription.substring(1).toLowerCase();
