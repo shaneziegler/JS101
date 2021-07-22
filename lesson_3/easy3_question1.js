bbc7c06a6c69a04b6ae6df739f90373173c94c34
Shane-Ziegler <shanedb@gmail.com>
2021-07-22 08:23:36 -0500

    Thursday morning

---
+++ lesson_3/easy3_question1.js
@@ -0,0 +1,9 @@
+// Write three different ways to remove all of the elements from the following array:
+
+let numbers = [1, 2, 3, 4];
+
+// numbers = [];
+
+// pop or shift them all off
+
+// numbers.length = 0;
