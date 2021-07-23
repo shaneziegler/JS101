c822d132d22232cc26d3ef0e6bc0519b2327b58e
Shane-Ziegler <shanedb@gmail.com>
2021-07-23 06:22:26 -0500

    Done

--- lesson_3/easy3_question1.js
+++ lesson_3/easy3_question1.js
@@ -7,3 +7,13 @@ let numbers = [1, 2, 3, 4];
 // pop or shift them all off
 
 // numbers.length = 0;
+
+
+numbers.length = 0;
+
+while (numbers.length > 0) {
+  numbers.pop();
+}
+
+numbers.splice(0, numbers.length);
+
