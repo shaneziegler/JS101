c8c96078a7d4406b749118ea0c58ef0f42d7edff
Shane-Ziegler <shanedb@gmail.com>
2021-07-23 07:06:41 -0500

    Done

---
+++ lesson_3/medium1_question1.js
@@ -0,0 +1,8 @@
+for (let i = 0; i < 10; i += 1) {
+  console.log("".padStart(i,) + 'The Flintstones Rock!');
+}
+
+// launch solution
+for (let padding = 1; padding <= 10; padding++) {
+  console.log(" ".repeat(padding) + "The Flinstones Rock!");
+}
