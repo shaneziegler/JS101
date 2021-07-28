9b9f8dc9cfc2bd1db77eb5642fbb6e58e19cb302
Shane-Ziegler <shanedb@gmail.com>
2021-07-28 07:47:46 -0500

    Done

--- exercises/greeting_a_user.js
+++ exercises/greeting_a_user.js
@@ -2,3 +2,13 @@
 // Easy 2
 // Greeting a user
 
+let readline = require('readline-sync');
+
+let name = readline.question('What is your name? ');
+
+if (name.charAt(name.length - 1) === '!') {
+  name = name.slice(0, -1);
+  console.log(`HELLO ${name.toLocaleUpperCase()}. WHY ARE WE SCREAMING?`);
+} else {
+  console.log(`Hello ${name}.`);
+}
