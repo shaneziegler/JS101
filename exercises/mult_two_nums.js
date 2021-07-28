9a7010d5c0089091436270317797e5c09a69bdaa
Shane-Ziegler <shanedb@gmail.com>
2021-07-28 07:51:23 -0500

    Done

---
+++ exercises/mult_two_nums.js
@@ -0,0 +1,11 @@
+// JS101
+// Easy 2
+// Multiplying two numbers
+
+// Create a function that takes two arguments, multiplies them together, and returns the result.
+
+function multiply(operand1, operand2) {
+  return operand1 * operand2;
+}
+
+console.log(multiply(5, 3) === 15); // logs true
