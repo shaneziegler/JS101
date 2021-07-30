d3645e0ca73fa18187e1571dff92fc83c022ea19
Shane-Ziegler <shanedb@gmail.com>
2021-07-29 22:49:05 -0500

    Done

---
+++ exercises/the_end_is_near.js
@@ -0,0 +1,18 @@
+// JS101
+// Small Problems
+// Easy 2
+// the end is near but not here
+
+
+// Write a function that returns the next to last word in the String passed to it as an argument.
+// Words are any sequence of non-blank characters.
+// You may assume that the input String will always contain at least two words.
+
+
+function penultimate(str) {
+  let arr = str.split(' ');
+  return arr[arr.length - 2];
+}
+
+console.log(penultimate("last word") === "last"); // logs true
+console.log(penultimate("Launch School is great!") === "is"); // logs true
