22fc6dcd683386c91336f842489cbbceea2d921a
Shane-Ziegler <shanedb@gmail.com>
2021-08-05 06:40:01 -0500

    Done

--- lesson_4/when_will_i_retire.js
+++ lesson_4/when_will_i_retire.js
@@ -1,3 +1,14 @@
 // JS 101
 // Easy 4
 // When will I retire?
+
+let readline = require('readline-sync');
+
+let currentAge = parseInt(readline.question('What is your age? '));
+let retireAge = parseInt(readline.question('At what age would you like to retire? '));
+
+let currentYear = new Date().getFullYear();
+let yearsOfWorkLeft = retireAge - currentAge;
+
+console.log(`It's ${currentYear}. You will retire in ${currentYear + yearsOfWorkLeft}.`);
+console.log(`You have only ${yearsOfWorkLeft} years of work to go!`);
