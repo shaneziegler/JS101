e3e2e6272ac07edf1dba8f52781ad14f925b2606
Shane-Ziegler <shanedb@gmail.com>
2021-08-09 10:44:27 -0500

    Fix comment desc

--- lesson_4/palindromic_number.js
+++ lesson_4/palindromic_number.js
@@ -1,6 +1,6 @@
 // JS 101
 // Easy 4
-// Palindromic Strings 2
+// Palindromic Strings
 
 // Write a function that returns true if the string passed as an argument is a palindrome, or false otherwise.
 // A palindrome reads the same forwards and backwards. For this problem, the case matters and all characters matter.
