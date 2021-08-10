88c3b59f6ff1a5171a27fa757b69d361f5bdd940
Shane-Ziegler <shanedb@gmail.com>
2021-08-10 07:06:01 -0500

    Tueday morning

---
+++ lesson_4/letter_swap.js
@@ -0,0 +1,15 @@
+// JS 101
+// Easy 4
+// Letter Swap
+
+// Given a string of words separated by spaces,
+// write a function that swaps the first and last letters of every word.
+// You may assume that every word contains at least one letter,
+// and that the string will always contain at least one word.
+// You may also assume that each string contains nothing but words and spaces,
+// and that there are no leading, trailing, or repeated spaces.
+
+
+swap('Oh what a wonderful day it is');  // "hO thaw a londerfuw yad ti si"
+swap('Abcde');                          // "ebcdA"
+swap('a');                              // "a"
