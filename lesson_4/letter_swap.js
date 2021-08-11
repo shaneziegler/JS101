d231bf07f196d7364c298a19367e76fe6f8d10e0
Shane-Ziegler <shanedb@gmail.com>
2021-08-11 07:15:07 -0500

    Works using strings, but ugly

--- lesson_4/letter_swap.js
+++ lesson_4/letter_swap.js
@@ -9,6 +9,19 @@
 // You may also assume that each string contains nothing but words and spaces,
 // and that there are no leading, trailing, or repeated spaces.
 
+function swap(str) {
+  let words = str.split(' ');
+  let swappedArr = words.map(word => {
+    let firstLetter = word[0];
+    let lastLetter = '';
+    if (word.length > 1) {
+      lastLetter = word[word.length - 1];
+    }
+    let newString = lastLetter + word.slice(1, word.length - 1) + firstLetter;
+    return newString;
+  });
+  return swappedArr.join(' ');
+}
 
 swap('Oh what a wonderful day it is');  // "hO thaw a londerfuw yad ti si"
 swap('Abcde');                          // "ebcdA"
