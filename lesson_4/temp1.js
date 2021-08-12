1eae1cb1d37205308b0df7db8e85bcb135b43037
Shane-Ziegler <shanedb@gmail.com>
2021-08-11 22:41:22 -0500

    Workspace

---
+++ lesson_4/temp1.js
@@ -0,0 +1,74 @@
+let produce = {
+  apple: 'Fruit',
+  carrot: 'Vegetable',
+  pear: 'Fruit',
+  broccoli: 'Vegetable'
+};
+
+selectFruit(produce); // => { apple: 'Fruit', pear: 'Fruit' }
+
+function selectFruit(produce) {
+  let arr = Object.entries(produce).filter(pair => pair[1] === 'Fruit');
+  return Object.fromEntries(arr);
+}
+
+
+
+function doubleNumbers(numbers) {
+  let counter = 0;
+
+  while (counter < numbers.length) {
+    numbers[counter] *= 2;
+    counter += 1;
+  }
+}
+
+let myNumbers = [1, 4, 3, 7, 2, 6];
+doubleNumbers(myNumbers); // => [2, 8, 6, 14, 4, 12]
+myNumbers;                // => [1, 4, 3, 7, 2, 6]
+
+
+function doubleOddNumbers(numbers) {
+  let doubledNums = [];
+
+  for (let counter = 0; counter < numbers.length; counter += 1) {
+    let currentNumber = numbers[counter];
+
+    if (counter % 2 === 1) {
+      doubledNums.push(currentNumber * 2);
+    } else {
+      doubledNums.push(currentNumber);
+    }
+  }
+
+  return doubledNums;
+}
+
+let myNumbers = [1, 4, 3, 7, 2, 6];
+doubleOddNumbers(myNumbers);  // => [2, 4, 6, 14, 2, 6]
+
+// not mutated
+myNumbers;                    // => [1, 4, 3, 7, 2, 6]
+
+
+function doubleNumbers(numbers) {
+  let counter = 0;
+
+  while (counter < numbers.length) {
+    numbers[counter] *= 2;
+    counter += 1;
+  }
+}
+
+function multiply(numbers, mult) {
+  let counter = 0;
+
+  while (counter < numbers.length) {
+    let currentNum = numbers[counter];
+    numbers[counter] = currentNum * mult;
+
+    counter += 1;
+  }
+
+  return numbers;
+}
