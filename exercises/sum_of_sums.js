04469f1d25e11d34fdc42fdb913b5ac1798c6c3b
Shane-Ziegler <shanedb@gmail.com>
2021-08-31 22:33:54 -0500

    Tues night2

---
+++ exercises/sum_of_sums.js
@@ -0,0 +1,16 @@
+// JS101
+// List Processing
+// Sum of sums
+
+// Write a function that takes an array of numbers and returns the sum of the sums of each leading subsequence in that array. 
+// Examine the examples to see what we mean. You may assume that the array always contains at least one number.
+
+sumOfSums([3, 5, 2]);        // (3) + (3 + 5) + (3 + 5 + 2) --> 21
+sumOfSums([1, 5, 7, 3]);     // (1) + (1 + 5) + (1 + 5 + 7) + (1 + 5 + 7 + 3) --> 36
+sumOfSums([4]);              // 4
+sumOfSums([1, 2, 3, 4, 5]);  // 35
+
+function sumOfSums(arr) {
+  let sum = 0;
+  return arr.map(elm => sum += elm).reduce((acc, elm) => acc + elm, 0);
+}
