507ce5a45354456c7c003663124ca606754bcc52
Shane-Ziegler <shanedb@gmail.com>
2021-09-01 06:55:24 -0500

    Wed morning

---
+++ exercises/grocery_list.js
@@ -0,0 +1,17 @@
+// js101
+// list processing
+// Grocery List
+
+// Write a function that takes a grocery list in a two-dimensional array and returns a one-dimensional array. 
+// Each element in the grocery list contains a fruit name and a number that represents the desired quantity of that fruit. 
+// The output array is such that each fruit name appears the number of times equal to its desired quantity.
+// In the example below, we want to buy 3 apples, 1 orange, and 2 bananas. 
+// Thus, we return an array that contains 3 apples, 1 orange, and 2 bananas.
+
+buyFruit([['apple', 3], ['orange', 1], ['banana', 2]]);
+// returns ["apple", "apple", "apple", "orange", "banana", "banana"]
+
+
+function buyFruit(arr) {
+  return arr.map((subArr) => [...Array(subArr[1])].map(value => subArr[0])).flat();
+}
---
+++ exercises/rotation_part1.js
@@ -0,0 +1,48 @@
+// Js101
+// Medium 1
+// Rotation 1
+
+// Write a function that rotates an array by moving the first element to the end of the array. 
+// Do not modify the original array.
+// If the input is not an array, return undefined.
+// If the input is an empty array, return an empty array.
+
+rotateArray([7, 3, 5, 2, 9, 1]);       // [3, 5, 2, 9, 1, 7]
+rotateArray(['a', 'b', 'c']);          // ["b", "c", "a"]
+rotateArray(['a']);                    // ["a"]
+rotateArray([1, 'a', 3, 'c']);         // ["a", 3, "c", 1]
+rotateArray([{ a: 2 }, [1, 2], 3]);    // [[1, 2], 3, { a: 2 }]
+rotateArray([]);                       // []
+
+// return `undefined` if the argument is not an array
+rotateArray();                         // undefined
+rotateArray(1);                        // undefined
+
+
+// the input array is not mutated
+let array = [1, 2, 3, 4];
+rotateArray(array);                    // [2, 3, 4, 1]
+array;                                 // [1, 2, 3, 4]
+
+function rotateArray(arr) {
+  if (!Array.isArray(arr)) return undefined;
+  if (arr.length === 0) return []; 
+
+  let newArr = arr.slice();
+  newArr.push(newArr.shift());
+  return newArr;
+
+}
+
+// Ls solution
+function rotateArray(array) {
+  if (!Array.isArray(array)) {
+    return undefined;
+  }
+
+  if (array.length === 0) {
+    return [];
+  }
+
+  return array.slice(1).concat(array[0]);
+}
