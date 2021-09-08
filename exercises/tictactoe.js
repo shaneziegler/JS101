dc1c2482b3fa87ef36d0999f50e7f70a8d534e97
Shane-Ziegler <shanedb@gmail.com>
2021-09-08 08:10:05 -0500

    wednesday morning

--- exercises/tictactoe.js
+++ exercises/tictactoe.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-lonely-if */
 /* eslint-disable max-lines-per-function */
 // JS101
 // Lesson 6
@@ -131,3 +132,15 @@ while (true) {
 }
 
 prompt('Thanks for playing Tic Tac Toe!');
+
+
+function joinOr(arr, delimiter = ', ', conjunction = 'or') {
+  switch (arr.length) {
+    case 0: return "";
+    case 1: return String(arr[0]);
+    case 2: return arr.join(` ${conjunction} `);
+    default: {
+      return `${arr.slice(0, arr.length - 1).join(delimiter)}, ${conjunction} ${arr[arr.length - 1]}`; 
+    }
+  }
+}
