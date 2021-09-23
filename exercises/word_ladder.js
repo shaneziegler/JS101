5549e03aad31b188dad770a0aad103b700b4993b
Shane-Ziegler <shanedb@gmail.com>
2021-09-23 07:31:16 -0500

    test

--- exercises/word_ladder.js
+++ exercises/word_ladder.js
@@ -1,3 +1,15 @@
+// JS 101
+// Debugging JS
+// Word Ladders
+
+// A "ladder" is simply an array of word strings;
+// Gemma decides to transform this array into a single string where each word within the string is separated by a hyphen ('-').
+//  For example, the array ['pig', 'pie', 'lie', 'lit', 'let'] should be printed as the string 'pig-pie-lie-lit-let'.
+
+// Upon first glance,
+// Gemma's code below looks like it should work. But it throws a TypeError, saying: 
+// Cannot read property 'forEach' of undefined. Why is that?
+
 let ladder = ''
 
 ['head', 'heal', 'teal', 'tell', 'tall', 'tail'].forEach(word => {
@@ -9,4 +21,3 @@ let ladder = ''
 })
 
 console.log(ladder)  // expect: head-heal-teal-tell-tall-tail
-
