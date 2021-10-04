b210cbfddb50fec4712389fd4947cf5f84b12052
Shane-Ziegler <shanedb@gmail.com>
2021-10-04 08:24:39 -0500

    monday morning

---
+++ exercises/merge_sort.js
@@ -0,0 +1,13 @@
+// JS101
+// Advanced 1
+// Merge Sort
+
+mergeSort([9, 5, 7, 1]);           // [1, 5, 7, 9]
+mergeSort([5, 3]);                 // [3, 5]
+mergeSort([6, 2, 7, 1, 4]);        // [1, 2, 4, 6, 7]
+
+mergeSort(['Sue', 'Pete', 'Alice', 'Tyler', 'Rachel', 'Kim', 'Bonnie']);
+// ["Alice", "Bonnie", "Kim", "Pete", "Rachel", "Sue", "Tyler"]
+
+mergeSort([7, 3, 9, 15, 23, 1, 6, 51, 22, 37, 54, 43, 5, 25, 35, 18, 46]);
+// [1, 3, 5, 6, 7, 9, 15, 18, 22, 23, 25, 35, 37, 43, 46, 51, 54]
