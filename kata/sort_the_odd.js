f18b898a6d97d02fa191c02ecb2a3bcdff6b6970
Shane-Ziegler <shanedb@gmail.com>
2021-11-08 22:33:14 -0600

    Monday night

--- kata/sort_the_odd.js
+++ kata/sort_the_odd.js
@@ -1,11 +1,18 @@
 // kata
 
 function sortArray(arr) {
-  let odd = arr.filter(elm => elm % 2 === 1).sort((a,b) => a - b);
-  console.log(odd);
+  let odd = arr.filter(elm => Math.abs(elm % 2) === 1).sort((a,b) => a - b);
   return arr.map(elm => (elm % 2 === 0) ? elm : odd.shift());
 }
 
 // console.log(sortArray([9, 8, 7, 6, 5, 4, 3, 2, 1, 0]));
 // console.log(sortArray([]));
 console.log(sortArray([9, -8, 7, 6, 5, 4, 3, 2, -1, 0]));
+
+function sortArray2(arr) {
+  let odd = arr.filter(elm => Math.abs(elm % 2) === 1).sort((a,b) => a - b);
+  let oddSorted = odd.sort((a,b) => a - b);
+  console.log(odd);
+  console.log(oddSorted);
+  return arr.map(elm => (elm % 2 === 0) ? elm : odd.shift());
+}
