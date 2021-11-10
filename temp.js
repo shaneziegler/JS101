b23189ea660eabe32cd89e428d52b57a7e411405
Shane-Ziegler <shanedb@gmail.com>
2021-11-09 22:20:12 -0600

    tues night

--- temp.js
+++ temp.js
@@ -1,1 +1,5 @@
-// git test
+
+
+let scores = [[3, 6, 4], [6, 8, 9], [1, 4, 2], [0,0,0], [9,9,9]];
+let s = scores.sort((a, b) => a.reduce((c, d) => c + d) - b.reduce((x, y) => x + y));
+console.log(s);
