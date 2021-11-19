8448080f32e8775eae6ed854982d74afc529e324
Shane-Ziegler <shanedb@gmail.com>
2021-11-19 08:21:21 -0600

    Friday morning

--- exercises/anki.js
+++ exercises/anki.js
@@ -1,3 +1,8 @@
 let words = ['go', 'ahead', 'and', 'jump'];
+let flintstones = ["Fred", "Wilma"];
+flintstones.push(["Barney", "Betty"]);
+flintstones.push(["Bambam", "Pebbles"]);
 
-words.sort((a, b) => a.length - b.length);
+let newArr = flintstones.reduce((a, b) => a.concat(b), []);
+
+console.log(newArr);
