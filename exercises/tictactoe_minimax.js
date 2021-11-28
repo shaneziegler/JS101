e61f7742a0187cdbf1414b0305ea6aa03f48cb75
Shane-Ziegler <shanedb@gmail.com>
2021-11-28 10:49:50 -0600

    Done

--- exercises/tictactoe_minimax.js
+++ exercises/tictactoe_minimax.js
@@ -6,7 +6,7 @@
 const INITIAL_MARKER = ' ';
 const HUMAN_MARKER = 'X';
 const COMPUTER_MARKER = 'O';
-const MAX_WINS = 2;
+const MAX_WINS = 3;
 const WINNINGLINES = [
   [1, 2, 3], [4, 5, 6], [7, 8, 9], // rows
   [1, 4, 7], [2, 5, 8], [3, 6, 9], // columns
