edc400a7f25463e67a370385d956f5589419c7d9
Shane-Ziegler <shanedb@gmail.com>
2021-12-31 08:18:03 -0600

    Firday moprnign study session with alex and jeff

---
+++ exercises/a.js
@@ -0,0 +1,10 @@
+let something = []; // line 1
+let somethingElse = ''; // line 2
+
+if (something === somethingElse) { // line 4
+  console.log("TV");               // line 5
+} else if (something == somethingElse) { // line 6
+  console.log("Radio");                   // line 7
+} else { // line 8
+  console.log("Other"); // line 9
+} // line 10
