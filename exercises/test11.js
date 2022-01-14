0dcf19f79b2daa6f52cbb0836b34c98f9b9122f7
Shane-Ziegler <shanedb@gmail.com>
2022-01-13 18:03:43 -0600

    tesdint

---
+++ exercises/test11.js
@@ -0,0 +1,22 @@
+//
+
+let x = 5;
+
+const test2 = function (a) {
+  return a * 2;
+}
+
+const test3 = a => a * 2;
+
+console.log(test1(x));
+
+console.log(test2(x));
+
+console.log(test3(x));
+
+function test1(a) {
+  return a * 2;
+}
+
+
+
